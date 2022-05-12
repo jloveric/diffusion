62dc2807e9c07b3883cb8068c6728169f8588825
John <john.loverich@gmail.com>
2022-05-12 16:31:51 -0700

    Just trying to get something working

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # diffusion
+
+Borrowed from 
+https://www.assemblyai.com/blog/diffusion-models-for-machine-learning-introduction/
+
 Experiments in NN diffusion models
---
+++ diffusion.py
@@ -0,0 +1,42 @@
+from denoising_diffusion_pytorch import Unet, GaussianDiffusion, Trainer
+
+from torchvision import datasets
+from torchvision.transforms import ToTensor
+
+"""
+train_data = datasets.MNIST(
+    root="data",
+    train=True,
+    transform=ToTensor(),
+    download=True,
+)
+test_data = datasets.MNIST(root="data", train=False, transform=ToTensor())
+"""
+
+image_size = 32
+
+model = Unet(dim=64, dim_mults=(1, 2, 4, 8)).cuda()
+
+
+diffusion = GaussianDiffusion(
+    model,
+    image_size=image_size,
+    timesteps=1000,
+    loss_type="l1",  # number of steps  # L1 or L2
+).cuda()
+
+trainer = Trainer(
+    diffusion,
+    "/mnt/1000gb/cifar10/test/airplane",
+    image_size=image_size,
+    train_batch_size=32,
+    train_lr=2e-5,
+    train_num_steps=2,  # total training steps
+    gradient_accumulate_every=2,  # gradient accumulation steps
+    ema_decay=0.995,  # exponential moving average decay
+    amp=True,  # turn on mixed precision
+)
+
+trainer.train()
+
+sampled_images = diffusion.sample(batch_size=4)
